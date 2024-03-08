0e8bc448b406502a951eb4aa05ea1c6380dca87b
Alina <ruesullivanpro@gmail.com>
2024-03-08 21:47:37 +0400

    test: add chrome mock with local storage functions

---
+++ src/shared/browser/__mocks__/chrome.ts
@@ -0,0 +1,47 @@
+import { MockedObject, vi } from 'vitest'
+
+type IChrome = typeof chrome
+type StoreValue = Record<string, unknown>
+
+let store: StoreValue = {}
+
+function resolveKey(key: unknown) {
+  if (typeof key === 'string') {
+    const result: StoreValue = {}
+    result[key] = store[key]
+    return result
+  } else if (Array.isArray(key)) {
+    return key.reduce((acc, curr) => {
+      acc[curr] = store[curr]
+      return acc
+    }, {})
+  }
+  throw new Error('Wrong key given')
+}
+
+export const chromeBrowser = {
+  storage: {
+    local: {
+      get: vi.fn((id, cb) => {
+        const result = id === null ? store : resolveKey(id)
+        return cb ? cb(result) : Promise.resolve(result)
+      }),
+
+      set: vi.fn((payload, cb) => {
+        Object.keys(payload).forEach((key) => (store[key] = payload[key]))
+        return cb ? cb() : Promise.resolve()
+      }),
+
+      onChanged: {
+        addListener: vi.fn(),
+        removeListener: vi.fn(),
+        hasListener: vi.fn(),
+      },
+
+      clear: vi.fn((cb) => {
+        store = {}
+        return cb ? cb() : Promise.resolve()
+      }),
+    },
+  },
+} as unknown as MockedObject<IChrome>
