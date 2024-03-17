ea3583e4d25239c6be7762d5daf09355f3b74910
Alina <ruesullivanpro@gmail.com>
2024-03-17 23:04:02 +0400

    chore: add copy button

---
+++ src/shared/ui/CopyButton/index.tsx
@@ -0,0 +1,42 @@
+import { useRef, useState } from 'preact/hooks'
+
+import { browser } from '@shared/browser'
+
+import { Button, Props as ButtonProps } from '../Button'
+import { CopyIcon } from '../icons/CopyIcon'
+import { DoneIcon } from '../icons/DoneIcon'
+
+interface Props extends ButtonProps {
+  text: string
+}
+
+export const CopyButton = ({ text, ...rest }: Props) => {
+  const displayCopySuccessTimeout = useRef<NodeJS.Timeout>()
+  const [isCopied, setIsCopied] = useState(false)
+
+  const resetIsCopiedAfterDelay = () => {
+    clearTimeout(displayCopySuccessTimeout.current)
+    displayCopySuccessTimeout.current = setTimeout(() => {
+      setIsCopied(false)
+    }, 1000)
+  }
+
+  const onCopy = (text: string) => {
+    navigator.clipboard.writeText(text)
+    setIsCopied(true)
+    resetIsCopiedAfterDelay()
+  }
+
+  return (
+    <Button
+      {...rest}
+      startIcon={isCopied ? <DoneIcon /> : <CopyIcon />}
+      aria-label={
+        isCopied
+          ? browser.i18n.getMessage('copyTextCopiedButtonCaption')
+          : browser.i18n.getMessage('copyTextButtonCaption', text)
+      }
+      onClick={() => onCopy(text)}
+    />
+  )
+}
