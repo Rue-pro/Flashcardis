66ff40adefb4f1ef6209071cf35632fb88914b8a
Alina <ruesullivanpro@gmail.com>
2024-04-09 03:29:56 +0400

    chore: update operation result types

--- src/shared/libs/operationResult/index.ts
+++ src/shared/libs/operationResult/index.ts
@@ -1,12 +1,24 @@
+import { errorStore } from '@entities/error'
+
 export type TBaseError = { type: string; error: Error | null }
-export type TResult<T, E = TBaseError> =
+export type TResult<T = string, E = TBaseError> =
   | { data: T; error: null }
   | { data: null; error: E }
 
 export const Result = Object.freeze({
-  Success: <T, E>(data: T): TResult<T, E> => ({ data, error: null }),
-  Error: <T, E = TBaseError>(error: E): TResult<T, E> => ({
-    data: null,
-    error: error,
+  Success: <T = string, E extends TBaseError = TBaseError>(
+    data: T,
+  ): TResult<T, E> => ({
+    data,
+    error: null,
   }),
+  Error: <T = null, E extends TBaseError = TBaseError>(
+    error: E,
+  ): TResult<T, E> => {
+    errorStore.addError(error)
+    return {
+      data: null,
+      error,
+    }
+  },
 })
