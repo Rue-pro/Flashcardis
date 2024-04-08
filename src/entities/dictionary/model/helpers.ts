5608be4b6ff3e93d9d98585315b6892462e79e66
Alina <ruesullivanpro@gmail.com>
2024-04-09 03:57:01 +0400

    chore: add get dictionaries urls function

--- src/entities/dictionary/model/helpers.ts
+++ src/entities/dictionary/model/helpers.ts
@@ -1,7 +1,7 @@
 import { TLanguageCode } from '@entities/language'
 import { INoteSelectors } from '@entities/note'
 
-import { DICTIONARIES } from '.'
+import { DICTIONARIES, GOOGLE_TRANSLATE } from '.'
 import { IDictionary } from './types'
 
 export const getDictionaryByPageUrl = (
@@ -42,3 +42,29 @@ export const getSelectorsFromDictionary = (
 
   return dictionary.selectors
 }
+
+const getDictionariesUrls = (): string[] => {
+  const urls: string[] = []
+  const languages = Object.entries(DICTIONARIES)
+
+  let languageIndex = 0
+  while (languageIndex < languages.length) {
+    const languageDictionaries = languages[languageIndex][1].dictionaries
+
+    let dictionaryIndex = 0
+    while (dictionaryIndex < languageDictionaries.length) {
+      const dictionary = languageDictionaries[dictionaryIndex]
+
+      urls.push(dictionary.url + '*')
+
+      dictionaryIndex++
+    }
+    languageIndex++
+  }
+
+  urls.push(GOOGLE_TRANSLATE.url + '*')
+
+  return urls
+}
+
+export const dictionariesUrls = getDictionariesUrls()
