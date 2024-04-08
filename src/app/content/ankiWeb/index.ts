3a62400a86df2d82449ebffd24fbf0d8355a5456
Alina <ruesullivanpro@gmail.com>
2024-04-09 03:58:57 +0400

    chore: remove unused code

--- src/app/content/ankiWeb/index.ts
+++ src/app/content/ankiWeb/index.ts
@@ -4,7 +4,6 @@ import { browser } from '@shared/browser'
 
 import { fillForm } from './helpers/fillForm'
 
-console.log('anki web content code')
 browser.runtime.onConnect.addListener(function (port) {
   autoFillFormHandler(port, fillForm)
 })
