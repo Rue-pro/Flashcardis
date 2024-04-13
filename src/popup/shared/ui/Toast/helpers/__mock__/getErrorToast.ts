5fe8537b9533d0c50dabd0d5d144f40aac13b8ae
Alina <ruesullivanpro@gmail.com>
2024-04-13 23:45:02 +0400

    chore: remove toast component

--- src/popup/shared/ui/Toast/helpers/__mock__/getErrorToast.ts
+++
@@ -1,5 +0,0 @@
-import { MockedFunction, vi } from 'vitest'
-
-import { getErrorToast } from '../getErrorToast'
-
-export const getErrorToastMock: MockedFunction<typeof getErrorToast> = vi.fn()
--- src/popup/shared/ui/Toast/model/__mock__/store.ts
+++
@@ -1,5 +0,0 @@
-import { MockedFunction, vi } from 'vitest'
-
-import { addToast } from '../store'
-
-export const addToastMock: MockedFunction<typeof addToast> = vi.fn()
